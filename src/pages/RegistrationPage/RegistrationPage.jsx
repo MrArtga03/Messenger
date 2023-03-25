3faf8b703e68a18af1ed4c96b4855ef4f893fbb0
MrArtga <artga03@gmail.com>
2023-03-25 15:43:15 +0300

    MSNG-6: Условия валидации

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { QuestionIcon } from '@chakra-ui/icons'
+import { useForm } from 'react-hook-form'
 import { 
   CardBody, 
   Center, 
@@ -32,6 +33,20 @@ import styles from './RegistrationPage.module.scss'
 const RegistrationPage = () => {
   const [showPas, setPasShow] = useState(false)
   const [showRepPas, setRepPasShow] = useState(false)
+
+  const {
+    register,
+    formState: {
+      errors,
+    },
+    handleSubmit,
+  } = useForm({
+      mode: 'onBlur'
+    })
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data))
+  }
 
   const handleClickPassword = () => setPasShow(!showPas)
   const handleClickRepPassword = () => setRepPasShow(!showRepPas)
@@ -91,73 +106,111 @@ const RegistrationPage = () => {
           <Divider />
 
           <CardBody>
-            <Stack mt='3' spacing='3'>
-              <Input
-                variant={'flushed'} 
-                placeholder={'Full organization name'}
-                color={'#fff'}
-              />
-
-              <Input
-                variant={'flushed'} 
-                placeholder={'Short name'}
-                color={'#fff'}
-              />
-
-              <Input
-                variant={'flushed'} 
-                placeholder={'Email'}
-                color={'#fff'}
-              />
-
-              <InputGroup size='md'>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Stack mt='3' spacing='3'>
                 <Input
-                  pr='4.5rem'
-                  color={'#fff'}
+                  {...register('fullorgname', {
+                    required: 'Поле является обязательным!'
+                  })}
                   variant={'flushed'} 
-                  type={showPas ? 'text' : 'password'}
-                  placeholder={'Enter new password'}
+                  placeholder={'Full organization name'}
+                  color={'#fff'}
                 />
-                <InputRightElement width='4.5rem'>
-                  <Button h='1.75rem' size='sm' onClick={handleClickPassword}>
-                    {showPas ? 'Hide' : 'Show'}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
 
-              <InputGroup size='md'>
+                <Text h={'15px'}>
+                  {errors?.fullorgname && <p style={{color: 'red'}}>{errors?.fullorgname?.message || 'Вы должны написать полное имя организации!'}</p>}
+                </Text>
+
                 <Input
-                  pr='4.5rem'
-                  color={'#fff'}
+                  {...register('shortname', {
+                    required: 'Поле является обязательным!'
+                  })}
                   variant={'flushed'} 
-                  type={showRepPas ? 'text' : 'password'}
-                  placeholder={'Repeat password'}
+                  placeholder={'Short name'}
+                  color={'#fff'}
                 />
-                <InputRightElement width='4.5rem'>
-                  <Button h='1.75rem' size='sm' onClick={handleClickRepPassword}>
-                    {showRepPas ? 'Hide' : 'Show'}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </Stack>
-          </CardBody>
 
-          <Divider />
+                <Text h={'15px'}>
+                  {errors?.shortname && <p style={{color: 'red'}}>{errors?.shortname?.message || 'Вы должны написать короткое имя организации!'}</p>}
+                </Text>
 
-          <CardFooter>
-          <HStack >
-              <Button>Sing Up</Button>
-              <Divider orientation={'vertical'} />
+                <Input
+                  {...register('email', {
+                    required: 'Поле является обязательным!'
+                  })}
+                  variant={'flushed'} 
+                  placeholder={'Email'}
+                  color={'#fff'}
+                />
 
-              <Stack>
-                <Text color={'#9d9d9d59'}>
-                  <Text as='ins'>
-                    <CustomLink to='/auth'>Log In</CustomLink>
-                  </Text>
+                <Text h={'15px'}>
+                  {errors?.email && <p style={{color: 'red'}}>{errors?.email?.message || 'Вы должны написать ваш email!'}</p>}
+                </Text>
+
+                <InputGroup size='md'>
+                  <Input
+                    {...register('newpass', {
+                      required: 'Поле является обязательным!',
+                      minLength: {
+                        value: 5,
+                        message: 'Минимум 5 символов!'
+                      }
+                    })}
+                    pr='4.5rem'
+                    color={'#fff'}
+                    variant={'flushed'} 
+                    type={showPas ? 'text' : 'password'}
+                    placeholder={'Enter new password'}
+                  />
+                  <InputRightElement width='4.5rem'>
+                    <Button h='1.75rem' size='sm' onClick={handleClickPassword}>
+                      {showPas ? 'Hide' : 'Show'}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+
+                <Text h={'15px'}>
+                  {errors?.newpass && <p style={{color: 'red'}}>{errors?.newpass?.message || 'Вы должны написать новый пароль!'}</p>}
+                </Text>
+
+                <InputGroup size='md'>
+                  <Input
+                    {...register('reppass', {
+                      required: 'Поле является обязательным!'
+                    })}
+                    pr='4.5rem'
+                    color={'#fff'}
+                    variant={'flushed'} 
+                    type={showRepPas ? 'text' : 'password'}
+                    placeholder={'Repeat password'}
+                  />
+                  <InputRightElement width='4.5rem'>
+                    <Button h='1.75rem' size='sm' onClick={handleClickRepPassword}>
+                      {showRepPas ? 'Hide' : 'Show'}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+
+                <Text h={'15px'}>
+                  {errors?.reppass && <p style={{color: 'red'}}>{errors?.reppass?.message || 'Вы должны повторно написать пароль!'}</p>}
                 </Text>
               </Stack>
-            </HStack>
-          </CardFooter>
+
+              <Divider mt={'4px'}/>
+
+              <HStack mt={'10px'} >
+                <Button type={'submit'}>Sing Up</Button>
+                <Divider orientation={'vertical'} />
+                <Stack>
+                  <Text color={'#9d9d9d59'}>
+                    <Text as='ins'>
+                      <CustomLink to='/auth'>Log In</CustomLink>
+                    </Text>
+                  </Text>
+                </Stack>
+              </HStack>
+            </form>
+          </CardBody>
         </Card>
       </div>
     </section>
