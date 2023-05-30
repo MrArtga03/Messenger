9d00ec5f42bbff8d79564510801f4fc145e5be07
MrArtga <artga03@gmail.com>
2023-05-30 08:56:36 +0300

    MSNGR-12: PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,15 @@
-// Название кэша - для каждого изменения кода стоит переименовывать
 var CACHE = 'cache'
 
-// Отлавливаем событие установки воркера
 self.addEventListener('install', function (evt) {
   evt.waitUntil(precache())
 })
 
-// На событии fetch используем кэш, но обновляем при появлении нового контента
 self.addEventListener('fetch', function (evt) {
   console.log('The service worker is serving the asset.')
   evt.respondWith(fromCache(evt.request))
   evt.waitUntil(update(evt.request))
 })
 
-// Записываем, что конкретно нам нужно кэшировать
 function precache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
@@ -28,7 +24,6 @@ function precache() {
   })
 }
 
-// При запросе проверяем, есть ли в кэше нужный ресурс. Если да, отдаем кэш
 function fromCache(request) {
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
@@ -37,8 +32,6 @@ function fromCache(request) {
   })
 }
 
-// Обновление состоит из открытия кэша, обработки сетевых запросов
-// и сохранения новых данных
 function update(request) {
   return caches.open(CACHE).then(function (cache) {
     return fetch(request).then(function (response) {
